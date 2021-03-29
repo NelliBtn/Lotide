142c130da8aff43db7bcd4d5119076699886eaaf
Nelli Butenko <butenkonelly93@gmail.com>
2021-03-29 22:33:32 +0000

    Colourful and verbose Assert Function

---
+++ assertEqual.js
@@ -0,0 +1,23 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅ Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('🚫🚫🚫 Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual('Light', 'Light');
+assertEqual(5, 5);
+assertEqual('5', 5);
+
+
+/*
+If the values match, it should print (console.log) the following:
+Assertion Passed: [actual] === [expected] (but with the values filled in)
+
+Otherwise it should print(console.log) the following:
+Assertion Failed: [actual] !== [expected](but with the values filled in)
+*/
