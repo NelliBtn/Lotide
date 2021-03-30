abd903e5fd5df39a2faf35b149c9e229e380509a
Nelli Butenko <butenkonelly93@gmail.com>
2021-03-30 22:12:01 +0000

    Improved the code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -6,21 +6,31 @@
 //   }
 // };
 
-const assertArraysEqual = function(arr1, arr2) {
-  for (let x = 0; x < arr1.length; x++) {
-    if (arr1[x] !== arr2[x]) {
-      console.log(`🚫🚫🚫 Assertion Failed: ${arr1} !== ${arr2}`);
+const eqArrays = function (array1, array2) {
+  for (let x = 0; x < array1.length; x++) {
+    if (array1[x] !== array2[x]) {
+      return false;
     }
-  } console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } return true;
+};
 
-}
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 
-// const eqArrays = function (array1, array2) {
-//   for (let x = 0; x < array1.length; x++) {
-//     if (array1[x] !== array2[x]) {
-//       return false;
+
+// const assertArraysEqual = function (arr1, arr2) {
+//   for (let x = 0; x < arr1.length; x++) {
+//     if (arr1[x] !== arr2[x]) {
+//       console.log(`🚫🚫🚫 Assertion Failed: ${arr1} !== ${arr2}`);
 //     }
-//   } return true;
-// };
+//   } console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+
+// }
+
