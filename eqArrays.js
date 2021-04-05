f79c7b383b98b74501a112439d2c3fa09fd83b27
Nelli Butenko <butenkonelly93@gmail.com>
2021-04-05 18:34:12 +0000

    eqArrays function exporting itself

---
+++ eqArrays.js
@@ -0,0 +1,11 @@
+const assertEqual = require('./assertEqual');
+
+const eqArrays = function (array1, array2) {
+  for (let x = 0; x < array1.length; x++) {
+      if (array1[x] !== array2[x]) {
+        return false;
+      }
+  } return true;
+};
+
+module.exports = eqArrays;
